0e64532af6485c310174cafb6113b218c3ae8326
Emerson McComb <emerson.mc.comb@hotmail.com>
2018-01-21 19:01:53 +0000

    Style error, update, destroy and new messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
 
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render "new"
@@ -30,7 +30,7 @@ class ArticlesController < ApplicationController
     
     
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -46,7 +46,7 @@ class ArticlesController < ApplicationController
     @article.destroy
     
     redirect_to articles_path
-    flash[:notice] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"
     
   end
   
